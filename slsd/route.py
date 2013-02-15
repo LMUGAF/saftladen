825e1bccab217e3d03169aea01c777f33a350f56
Skruppy <skrupellos.spam@web.de>
2013-02-15 12:50:23 +0100

    Added comments and default domains to router

--- slsd/route.py
+++ slsd/route.py
@@ -26,29 +26,33 @@ class Router:
 				
 				raise Exception()
 		
-		## Now we can savely add the route
-		self.__routes.append(route)
+		## Now we can safely add the route
+		if route.isDefaultDomain():
+			self.__routes.append(route)
+		else:
+			self.__routes.insert(0, route)
 	
 	
 	def removeRoute(self, route):
 		self.__routes.remove(route)
 	
 	
+	def addRoutes(self, routes):
+		for (scanners, domain, port) in routes:
+			self.addRoute(Route(scanners, domain, port))
+	
+	
 	@synchronized_method
 	def scanned(self, scan):
-		found = False
-		
 		for route in self.__routes:
 			if route.match(scan):
-				found = True
-				
 				ret = route.call(scan)
 				
 				if ret == ERoutingResult.STOP_OK:
 					pass
 				
 				elif ret == ERoutingResult.STOP_FAIL:
-					Log.log(Log.LVL_WARN, Log.SRV_ROUTE, "The port faild")
+					Log.log(Log.LVL_WARN, Log.SRV_ROUTE, "The port failed")
 				
 				elif ret == ERoutingResult.NEXT:
 					Log.log(
@@ -63,49 +67,82 @@ class Router:
 						"The port returned an invalid return code (%s)" % ret
 					)
 				
-				break
+				return
 		
-		if not found:
-			Log.log(
-				Log.LVL_NOTICE, Log.SRV_ROUTE,
-				"No route for %s" % scan
-			)
+		Log.log(Log.LVL_NOTICE, Log.SRV_ROUTE, "No route for %s" % scan)
 
 
 
 class Route:
-	def __init__(self, scanner, domain, port):
-		self.__scanner = scanner
-		self.__domain  = domain
-		self.__port    = port
+	##
+	# A route maps scans to ports. The routing is based on the attributes of a
+	# scan. There can be only one matching route (so a port can't influence the
+	# routing by asking the router to pick the next matching route). A route
+	# defines a partition for each scan attribute.
+	# 
+	# @param scanners A list of scanners which a scan has to be in to match the
+	#                 route.
+	#
+	# @param domain A String the scanned code has to be prefix with to match the
+	#               route. "" is the default domain, if no other matches.
+	#
+	# @param port A callback function to be called with the remaining code
+	#             (truncated by the domain) if the rout matches.
+	def __init__(self, scanners, domain, port):
+		self.__scanners = frozenset(scanners)
+		self.__domain   = domain
+		self.__port     = port
 	
 	
-	## Test if the two routes would match the same scan
+	def isDefaultDomain(self):
+		return self.__domain == ""
+	
+	
+	##
+	# Tests if the two routes would match the same scan. In other words it tests
+	# whether the cross products of the defined partitions (of scan attributes)
+	# overlap.
+	# 
+	# @param The other route to match against.
+	# 
+	# @return True if this route and the parameter's route would match the same
+	#         scan, otherwise False.
 	def isDuplicate(self, route):
 		return (
-			(
-				route.__scanner is None or 
-				self.__scanner  is None or 
-				route.__scanner == self.__scanner
+			( # The routes have overlapping scanners
+				len(route.__scanners & self.__scanners) > 0
 			) and
-			(
-				route.__domain.startswith(self.__domain) or 
-				self.__domain.startswith(route.__domain)
+			( # The routes have overlapping domains
+				(
+					route.__domain == "" and self.__domain == ""
+				) or
+				(
+					(
+						route.__domain.startswith(self.__domain) or 
+						self.__domain.startswith(route.__domain)
+					) and
+					route.__domain != "" and
+					self.__domain  != ""
+				)
 			)
 		)
 	
 	
+	##
+	# Test if the scan matches this route.
 	def match(self, scan):
 		return (
-			scan.getCode().startswith(self.__domain) and
-			(
-				self.__scanner is None or
-				self.__scanner == scan.getScanner() 
-			)
+			scan.getScanner() in self.__scanners and
+			scan.getCode().startswith(self.__domain)
 		)
 	
 	
-	## Call the port with the scanned code without the domain prefix
+	##
+	# Call the port with the scanned code truncated by the domain.
+	# 
+	# @param The scan containing the code for the port.
+	# 
+	# @return The return value of the port which has to be from ERoutingResult.
 	def call(self, scan):
 		return self.__port(scan.getCode()[len(self.__domain):])
 
