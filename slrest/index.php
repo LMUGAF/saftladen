7e5aa7fab0f5e647444ce076cc14ff08a4f6de87
Skrupellos <skrupellos.spam@web.de>
2014-11-28 00:12:01 +0100

    slrest unicode fix for old PHP versions

--- slrest/index.php
+++ slrest/index.php
@@ -4,7 +4,6 @@ error_reporting(E_ALL);
 ini_set('display_errors', True);
 
 
-
 require_once("conf.inc");
 require_once("vendor/autoload.php");
 
@@ -31,7 +30,20 @@ $response = $resource->exec();
 
 // encode output
 if ($request->contentType == 'application/json') {
+	if(is_array($response->body)) {
+		array_walk_recursive(
+			$response->body,
+			function (&$item, $key) {
+				if(is_string($item)) $item = utf8_encode($item);
+			}
+		);
+	}
+	
 	$response->body = json_encode($response->body);
+/*
+	var_dump(mb_detect_encoding($response->body['name']));
+	$response->body = json_encode($response->body, JSON_UNESCAPED_UNICODE);
+	var_dump($response->body);*/
 }
 $response->contentType = $request->contentType;
 
